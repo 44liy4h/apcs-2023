503f448a1791948e3339aa3ade9d1c4f0a3de058
Aaliyah Hall <hallaal000@stu.asd103.org>
2023-04-10 14:25:38 -0700

    hi

--- unit10/Scramble.java
+++ unit10/Scramble.java
@@ -3,8 +3,17 @@ package unit10;
 import java.util.Arrays;
 
 public class Scramble {
+    // added code
+    for(
+    int i = 0;i<arr.length;i++)
+    {
+        System.out.print(arr[i] + "");
+    }System.out.println();
+
+   bubbleSort(arr);
 
     /**
+     
      * Sorts a provided array using bubble sort.
      * Algorithm: loop through the array with two nested loops.
      * - compare pairs of adjacent elements; swap them if necessary.
