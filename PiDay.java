3ada023daf77f53845e68db83af4b9d52d2561cd
44liy4h <127797281+44liy4h@users.noreply.github.com>
2023-04-21 22:27:49 +0000

    fixed piday

--- PiDay.java
+++ PiDay.java
@@ -2,15 +2,15 @@ public class PiDay {
 
     // infinite series for π
     // https://en.wikipedia.org/wiki/Pi#Rate_of_convergence
-    public static double piDay(int n, boolean subtract) {
-      // TODO
-    }
+    // public static double piDay(int n, boolean subtract) {
+    // TODO
+    // }
 
     public static void main(String args[]) {
-    double pi = calculatePi(1);
+        double pi = calculatePi(500);
         System.out.println("The value of pi is: " + pi);
     }
-   
+
     /**
      * @param n
      * @return
@@ -19,18 +19,20 @@ public class PiDay {
         if (n == 0) {
             return 4.0;
         } else {
-            double term = 1.0 / ((2 * n) + 1);
+            double term = 4.0 / ((2 * n) + 1);
             if (n % 2 == 0) {
-                return calculatePi(n-1) + term;
+                return calculatePi(n - 1) + term;
             } else {
-                return calculatePi(n-1) - term;
+                return calculatePi(n - 1) - term;
             }
-        //  Gregory–Leibniz
-        //double pi = piDay(1, false);
+            // Gregory–Leibniz
+            // double pi = piDay(1, false);
 
-        // Nilakantha
-        double pi = 3.0 + piDay(3, false);
+            // Nilakantha
+            // double pi = 3.0 + piDay(3, false);
 
-        System.out.println("pi = " + pi);
+            // System.out.println("pi = " + pi);
+        }
     }
+
 }
